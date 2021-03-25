f2289c87f204ef50c8cf838696f019485dd3b82a
Yury Smolka <49496382+YurySm@users.noreply.github.com>
2021-03-25 17:33:37 +0300

    Add files via upload

---
+++ precache-manifest.bec16d51f384bd504d30ce19e543c06a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14da8c512aeedc2e973fe76b148ee4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d86d49a66b2a309e5f1",
+    "url": "/static/css/main.b3fb4f24.chunk.css"
+  },
+  {
+    "revision": "c176b4509c5b9e6f09f9",
+    "url": "/static/js/2.6b9c0686.chunk.js"
+  },
+  {
+    "revision": "9d86d49a66b2a309e5f1",
+    "url": "/static/js/main.278ae31b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
